5d664d10e12bae5aa58784871999a87b04847624
Harshitha1r <harshithar2009@gmail.com>
2024-10-19 16:22:38 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
       setActivelight(lights[activeLight].next)
     }, lights[activeLight].time*1000);
   },[activeLight])
+
   return (
     <div>
       <h1>Todo List</h1>
@@ -29,12 +30,12 @@ const App = () => {
       <button onClick={()=>setActivelight("yellow")}>Yellow</button>
       <button onClick={()=>setActivelight("green")}>Green</button>
       <div style={{marginBottom:'1rem',height:'3rem',width:'3rem',backgroundColor:'red',marginLeft:'2rem',borderRadius:'2rem',
-        opacity:`${activeLight == "red" ? 1 :'0.2'}`
+        opacity:`${activeLight === "red" ? 1 :'0.2'}`
       }}></div>
       <div style={{marginBottom:'1rem',height:'3rem',width:'3rem',backgroundColor:'yellow',
-        opacity:`${activeLight == "yellow" ? 1 :'0.2'}`,marginLeft:'2rem',borderRadius:'2rem'}}></div>
+        opacity:`${activeLight === "yellow" ? 1 :'0.2'}`,marginLeft:'2rem',borderRadius:'2rem'}}></div>
       <div style={{marginBottom:'1rem',height:'3rem',width:'3rem',
-        opacity:`${activeLight == "green" ? 1 :'0.2'}`,backgroundColor:'green',marginLeft:'2rem',borderRadius:'2rem'}}></div>
+        opacity:`${activeLight === "green" ? 1 :'0.2'}`,backgroundColor:'green',marginLeft:'2rem',borderRadius:'2rem'}}></div>
     </div>
   );
 };
